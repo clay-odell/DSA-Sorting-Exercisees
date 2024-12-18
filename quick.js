90925294dd035d2c06e72149c2310af3824bc8d0
Clay O'Dell <clay.odell.music@gmail.com>
2024-12-18 15:23:18 -0600

    implemented pivot and quickSort

--- quick.js
+++ quick.js
@@ -3,12 +3,31 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start = 0, end = arr.length - 1){
+    let pivotElement = arr[start];
+    let swapIdx = start;
+    for (let i = start + 1; i <= end; i++ ) {
+        if (arr[i] < pivotElement) {
+            swapIdx++;
+            [arr[i], arr[swapIdx]] = [arr[swapIdx], arr[i]];
+        }
+    }
+    [arr[start], arr[swapIdx]] = [arr[swapIdx], arr[start]];
+    return swapIdx;
+}
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIdx = pivot(arr, left, right);
 
-module.exports = quickSort;
+        quickSort(arr, left, pivotIdx -1);
+        quickSort(arr, pivotIdx + 1, right);
+    }
+    return arr;
+}
+
+module.exports = {quickSort, pivot};
